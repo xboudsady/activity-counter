f15c2212be329f5be96c46257477feb16bd4e1d6
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-05-11 09:08:03 +0800

    Added Whoops404.js to render 404 error. Updated render() route in index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import './stylesheets/ui.scss';
 import { App } from './components/App';
+import { Router, Route, hashHistory, } from 'react-router';
 
 window.React = React;
 
 render(
-	<App />,
+	<Router history="hashHistory">
+		<Route path="/" components={App} />
+		<Route path="*" components={Whoops404} />
+	</Router>,
 	document.getElementById('react-container')
 )
