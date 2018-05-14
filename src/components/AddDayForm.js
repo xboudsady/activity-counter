64797b0fa0499ee732d88d34602f4f54ade587dc
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-05-14 11:07:53 +0800

    Added Autocomplete and background image

--- src/components/AddDayForm.js
+++ src/components/AddDayForm.js
@@ -1,4 +1,42 @@
-import { PropTypes } from 'react';
+import { PropTypes, Component } from 'react';
+
+const tahoeResorts = [
+    "Alpine Meadows",
+    "Boreal",
+    "Diamond Peak",
+    "Donner Ski Ranch",
+    "Heavenly",
+    "Homewood",
+    "Kirkwood",
+    "Mt. Rose",
+    "Northstar",
+    "Squaw Valley",
+    "Sugar Bowl"
+];
+
+class AutoComplete extends Component {
+    get value() {
+        return this.refs.inputResort.value;
+    }
+
+    set value(inputValue) {
+        this.refs.inputResort.value = inputValue;
+    }
+    
+    render () {
+        return (
+            <div>
+                <input ref="inputResort" type="text" list="tahoe-resorts" />
+                <datalist id="tahoe-resorts">
+                    {this.props.options.map(
+                        (opt, i) => 
+                        <option key={i}>{opt}</option>)}
+                </datalist>
+            </div>
+        )
+    }
+}
+
 
 export const AddDayForm = ({ resort, date, powder, backcountry, onNewDay }) => {
     
@@ -21,10 +59,7 @@ export const AddDayForm = ({ resort, date, powder, backcountry, onNewDay }) => {
     return (
         <form onSubmit={submit} className="add-day-form">
             <label htmlFor="resort">Resort Name</label>
-            <input id="resort" 
-                    type="text" 
-                    required
-                    defaultValue={resort}
+            <AutoComplete options={tahoeResorts}
                     ref={input => _resort = input} />
 
             <label htmlFor="date">Date</label>
